7fa99a60e7177ea4f3e1ed41b520997f4f0e001d
milkcoco <lin0968329658@gmail.com>
2023-12-27 12:45:06 +0800

    修改my app

--- Myapp/ui.r
+++ Myapp/ui.r
@@ -1,18 +1,20 @@
 library(shiny)
 
-# Define UI for application that plots random distributions 
+# Define UI for application that calculate the needed sample size 
 shinyUI(pageWithSidebar(
   
   # Application title
-  headerPanel("Hello Shiny!"),
+  headerPanel("Sample size calculator"),
   
-  # Sidebar with a slider input for number of observations
+  # Sidebar with numeric inputs for significance level, expected rate, acceptable error range.
   sidebarPanel(
-    sliderInput("obs", "Number of observations:", min = 0, max = 1000, value = 500)
+    numericInput("Sig", "Two-sided significance level:", min = 0, max = 1.000, value = 0.05),
+    numericInput("p", "The expected rate:", min = 0, max = 1, value = 0.5),
+    numericInput("X", "Acceptable error range:", min = 0, max = 1, value = 0.03)
   ),
   
-  # Show a plot of the generated distribution
+  # Show the outcome
   mainPanel(
-    plotOutput("distPlot")
+    h3(textOutput("Size"))
   )
 ))
